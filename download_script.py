ff557eb00ecb16b0f5f28dfd0a71cb31bc38ad29
NojoudAlshehri <111287779+NojoudAlshehri@users.noreply.github.com>
2024-03-27 22:08:14 +0100

    Delete

--- download_script.py
+++
@@ -1,8 +0,0 @@
-import vrplib
-
-names = ['E-n22-k4']
-
-for name in names: 
-    # Download an instance and a solution file 
-    vrplib.download_instance(name, "./instances/E/") 
-    vrplib.download_solution(name, "./instances/E")
